328d50f804d129f158b70745f12571181b487dd1
jess-s55 <122399556+jess-s55@users.noreply.github.com>
2023-01-13 11:35:05 -0500

    more questions in main

--- Main.java
+++ Main.java
@@ -53,12 +53,61 @@ public class Main {
                                         "for (int i = 1; i < size-1; i++)",
                                         "for (int i = 0; i < size; i++)",
                                         "for (int i = 1; i < size; i++)",
-                                        "for (int i = 2; i < size-1; i++)")
+                                        "for (int i = 2; i < size-1; i++)"),
+                                new QBuilder(String.format("Which of the following will execute only if both are true?%nx = 5;%ny = 7;"),
+                                        "b",
+                                        "if (x < 10 || y > 7)",
+                                        "if (x < 10 && y < 10)",
+                                        "if (x > 10 || y < 10)",
+                                        "if (x > 0 && y < 3)"),
+                                new QBuilder("Which of the following will instantiate an array with 5 integers?",
+                                        "a",
+                                        "int[] myArray = new int[5];",
+                                        "int[] myArray = new myArray[5];",
+                                        "int[] myArray = new int[]{5};",
+                                        "int{} myArray = new int{5};"),
+                                new QBuilder(String.format("What are the values of a and b?%nint i = 1;%nint a = i++;%nint b = ++i;"),
+                                        "c",
+                                        "a = 2, b = 3",
+                                        "a = 1, b = 2",
+                                        "a = 1, b = 3",
+                                        "a = 2, b = 2"),
+                                new QBuilder(String.format("What is the output of the following code?%nint a = 2;%nint b = 3;%nint c = 4;%nint d = 5;%nSystem.out.println((--b * a) + (c * d--));"),
+                                        "d",
+                                        "22",
+                                        "40",
+                                        "20",
+                                        "24"),
+                                new QBuilder(String.format("Which of the following is the correct way to create a constructor for the code below?%npublic class Person {%n    private int personId;"),
+                                        "b",
+                                        String.format("public int Person(int personId) {%nthis.personId = personId;%n}"),
+                                        String.format("public Person(int personId) {%nthis.personId = personId;%n}"),
+                                        String.format("public Person(int personId) {%npersonId = this.personId;%n}"),
+                                        String.format("public int Person(personId) {%nint this.personId = personId;%n}")),
+                                new QBuilder("What is it called when you have multiple constructors with the same name but different parameters in a class?",
+                                        "c",
+                                        "Overriding",
+                                        "Instantiating",
+                                        "Overloading",
+                                        "Nothing. It's impossible to have multiple constructors in the same class."),
+                                new QBuilder("What is the proper syntax for the main method?",
+                                        "d",
+                                        "public static main(String[] args) {",
+                                        "public void main(String args) {",
+                                        "public static void main(String args[]) {",
+                                        "public static void main(String[] args) {"),
+                                new QBuilder("Which terminal command would you use to see the gits you have already commited and pushed?",
+                                        "a",
+                                        "git log",
+                                        "git status",
+                                        "git init",
+                                        "git commit")
                         )
                 );
         Questions questions = new Questions(new Scanner(new InputStreamReader(System.in, StandardCharsets.UTF_8)), qBuilders);
         System.out.println("Java Test");
-        System.out.println("Enter the letter of the correct answer to move on to the next question.");
+        System.out.println(String.format("Enter the letter of the correct answer to move on to the next question.%nThere is only one correct answer per question."));
+
         questions.question1();
 
     }
